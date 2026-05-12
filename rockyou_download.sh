c478d08d928936e35a0c266ebe4e90044a877e5b
RainBWP <114127654+RainBWP@users.noreply.github.com>
2026-05-11 21:05:19 -0600

    Funny script

---
+++ rockyou_download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Download this file , 130MB
+
+wget https://weakpass.com/download/90/rockyou.txt.gz
+
+# Unzip the file
+
+gunzip rockyou.txt.gz
+
+# Ready for use in password cracking tools 
+
